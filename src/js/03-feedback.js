8cc26b72ca99eb94ea2c9115243c0469c38385b2
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-31 01:15:13 +0200

    email & message

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,4 +2,7 @@ import throttle from "lodash.throttle";
 
 const feedbackForm = document.querySelector(".feedback-form");
 
+feedbackForm.addEventListener('input', throttle(setLocalRecords, 500));
 
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
