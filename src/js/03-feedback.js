5c924a448b1c86394a6ac479e98f913da7e8ff84
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-31 23:54:29 +0200

    3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,43 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector(".feedback-form");
+// find form
+const feedbackForm = document.querySelector('.feedback-form');
 
-feedbackForm.addEventListener('input', throttle(setLocalRecords, 500));
+// add listener for input + throttle
+feedbackForm.addEventListener('input', throttle(localData, 500));
 
+// find inputs data
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
+
+// set data from input to local storage
+function localData() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+// function to get input data from local storage 
+function getLocalData() {
+  let localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localData !== null) {
+    email.value = localData.email;
+    message.value = localData.message;
+  }
+}
+
+// run fuction
+getLocalData();
+
+// add listener to submit form button
+feedbackForm.addEventListener('submit', submitData);
+
+// run function at submit and console log input data
+function submitData(e) {
+  e.preventDefault();
+  this.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
